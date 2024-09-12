5140640efa4a62188f32c2c6df165dd926ec1bb8
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-12 14:44:46 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -3,15 +3,15 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles, FallingEdge, Timer, ClockCycles
+from cocotb.triggers import ClockCycles
 
-segments = [ 63, 6, 91, 79, 102, 109, 125, 7, 127, 111, 94, 57, 118, 94, 123, 126]
 
 @cocotb.test()
-async def test_7seg(dut):
+async def test_project(dut):
     dut._log.info("Start")
 
-    clock = Clock(dut.clk, 1, units="ms")
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -20,14 +20,21 @@ async def test_7seg(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, 1000)
-        
-        # all bidirectionals are set to output
-        assert dut.uio_oe == 0xFF
+    # Set the input values you want to test
+    dut.ui_in.value = 20
+    dut.uio_in.value = 30
+
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    # assert dut.uo_out.value == 50
+
+    # Keep testing the module by changing the input values, waiting for
+    # one or more clock cycles, and asserting the expected output values.
