f5cb58aa20820cfcddcb9605cab92407f6775d69
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-12 09:18:15 -0700

    Update decoder.v

--- src/decoder.v
+++ src/decoder.v
@@ -12,32 +12,30 @@
 */
 
 module seg7 (
-    input wire [3:0] counter,
-    output reg [6:0] segments
+      input wire [3:0] digit,
+      output reg [6:0] segments
 );
 
-    always @(*) begin
-        case(counter)
-            0: segments = 7'b0111110; //U
-            1: segments = 7'b1110111; //A
-            2: segments = 7'b1111100; //B
-            3: segments = 7'b0111001; //C
-            4: segments = 7'b1000000; //-
-            5: segments = 7'b1111001; //E
-            6: segments = 7'b0111000; //L 
-            7: segments = 7'b1111001; //E
-            8: segments = 7'b0111001; //C
-            9: segments = 7'b0110001; //T
-            10: segments = 7'b1010000; //R
-            11: segments = 7'b0111111; //O
-            12: segments = 7'b1010100; //N
-            13: segments = 7'b0110000; //I
-            14: segments = 7'b0111001; //C
-            15: segments = 7'b1110111; //A
-            default:    
-                segments = 7'b0000000;
-        endcase
-    end
-
+      always @(*) begin
+            case (digit)
+            4'd0: segments = 7'b0111111; // A
+            4'd1: segments = 7'b0000110; // b
+            4'd2: segments = 7'b1011011; // C
+            4'd3: segments = 7'b1001111; // d
+            4'd4: segments = 7'b1100110; // E
+            4'd5: segments = 7'b1101101; // F
+            4'd6: segments = 7'b1111101; // G
+            4'd7: segments = 7'b0000111; // H
+            4'd8: segments = 7'b1111111; // I
+            4'd9: segments = 7'b1101111; // J
+            4'd10: segments = 7'b1011110; // K
+            4'd11: segments = 7'b0111001; // L
+            4'd12: segments = 7'b1110110; // M
+            4'd13: segments = 7'b1011110; // N
+            4'd14: segments = 7'b1111011; // O
+            4'd15: segments = 7'b1111110; // P
+            default: segments = 7'b0000000; // Blank (off)
+            endcase
+        end
 endmodule
 
