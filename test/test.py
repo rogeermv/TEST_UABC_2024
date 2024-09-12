fe0e8bfb1e62e47f9583d9292d03d796f633df39
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-12 13:21:10 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -7,11 +7,31 @@ from cocotb.triggers import ClockCycles, FallingEdge, Timer, ClockCycles
 
 segments = [ 63, 6, 91, 79, 102, 109, 125, 7, 127, 111, 94, 57, 118, 94, 123, 126]
 
+binary = [
+    '00111111',  # 63 en decimal
+    '00000110',  # 6 en decimal
+    '01011011',  # 91 en decimal
+    '01001111',  # 79 en decimal
+    '01100110',  # 102 en decimal
+    '01101101',  # 109 en decimal
+    '01111101',  # 125 en decimal
+    '00000111',  # 7 en decimal
+    '01111111',  # 127 en decimal
+    '01101111',  # 111 en decimal
+    '01011110',  # 94 en decimal
+    '00111001',  # 57 en decimal
+    '01110110',  # 118 en decimal
+    '01011110',  # 94 en decimal
+    '01111011',  # 123 en decimal
+    '01111110',  # 126 en decimal
+]
+
+
 @cocotb.test()
 async def test_7seg(dut):
     dut._log.info("Start")
 
-    clock = Clock(dut.clk, 0.1, units="us")
+    clock = Clock(dut.clk, 1, units="ms")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -25,10 +45,19 @@ async def test_7seg(dut):
 
     dut._log.info("Test project behavior")
 
-    for i in range(len(segments)):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, 10000)
-        actual_value = int(dut.uo_out.value)
-        dut._log.info(f"Actual uo_out value: {actual_value}")
-        assert actual_value == segments[i], f"Error: Expected {segments[i]}, but got {actual_value}"
+    for i in range(16):
+        dut._log.info("Check segment {}".format(i))
+        await ClockCycles(dut.clk, 1000)
+
+        # Convert the binary vector to decimal
+        binary_value = binary_vectors[i]
+        expected_decimal = segments[i]
+        converted_decimal = int(binary_value, 2)
+
+        # Check if the segment output matches the expected value
+        assert int(dut.segments.value) == converted_decimal
+        
+        # all bidirectionals are set to output
+        assert dut.uio_oe == 0xFF
+
 
