1f0960beacb520d6cb2a74af07e5376e3c2150f9
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-12 13:00:01 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles, FallingEdge, Timer, ClockCycles
 
-segments = [ 63, 6, 91, 79, 102, 109, 125, 7, 127, 111, 94, 57, 118, 94, 123, 126]
+segments = [ 6, 91, 79, 102, 109, 125, 7, 127, 111, 94, 57, 118, 94, 123, 126]
 
 @cocotb.test()
 async def test_7seg(dut):
@@ -27,6 +27,6 @@ async def test_7seg(dut):
 
     for i in range(15):
         dut._log.info("check segment {}".format(i))
-        assert int(dut.segments.value) == segments[i]
         await ClockCycles(dut.clk, 1000)
+        assert int(dut.segments.value) == segments[i]
 
