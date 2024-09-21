436890fe6ccad5efd55aca9deb4f790c3a961089
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-20 22:14:12 -0700

    Update tt_um_uabc_test2024.v

--- src/tt_um_uabc_test2024.v
+++ src/tt_um_uabc_test2024.v
@@ -39,9 +39,7 @@ module tt_um_uabc_test2024 (
     seg7 seg7(.digit(display_value), .segments(segment_reg));
     
   assign uo_out = segment_reg; 
-  assign uio_out = 0; 
-  assign uio_oe = 8'hFF; 
 
   // List all unused inputs to prevent warnings
-  //wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena,ui_in,uio_in,uio_oe,uio_out};
 endmodule
